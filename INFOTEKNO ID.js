b3c8ab86c6c030d46fb76b228e57d339f55b1a30
akhdanmj <49373342+akhdanmj@users.noreply.github.com>
2019-06-21 16:09:05 +0800

    Add files via upload

---
+++ INFOTEKNO ID.js
@@ -0,0 +1,2 @@
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v3.3&appId=627041661148030&autoLogAppEvents=1"></script>
